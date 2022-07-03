f2eef00e62a99e90d91b8c0abbdae72f2fea1545
leerohrer1 <loginlee3@gmail.com>
2022-07-03 11:53:02 -0500

    Added mock web API

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -2,20 +2,27 @@ import { Injectable } from '@angular/core';
 import { Student } from './student';
 import { STUDENTS } from './mock-sudent-data';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StudentService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  private studentsUrl = 'api/students';
 
   getStudents(): Observable<Student[]> {
-    const students = of(STUDENTS);
-    return students;
+    return this.http.get<Student[]>(this.studentsUrl);
   }
 
   getStudent(name: string): Observable<Student> {
-    const student = STUDENTS.find(s => s.name === name)!;
+    const student = STUDENTS.find((s) => s.name === name)!;
     return of(student);
   }
+
+  // updateStudent(student: Student): Student {
+  //   const index = this.getStudents().findIndex((e: Student) => e.name === student.name);
+  //   return this.getStudents().splice(index, 1, student);
+  // }
 }
